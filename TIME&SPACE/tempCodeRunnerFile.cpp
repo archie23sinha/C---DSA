5b4ba0f15544e2e0d7a64f89632d0f5786a77376
archie23sinha <archie23sinha@gmail.com>
2024-06-28 15:56:23 +0530

    new file:   TIME&SPACE/FirstOcc.cpp 	new file:   TIME&SPACE/FirstOcc.exe 	new file:   TIME&SPACE/tempCodeRunnerFile.cpp

---
+++ TIME&SPACE/tempCodeRunnerFile.cpp
@@ -0,0 +1,33 @@
+int first(int arr[] , int n, int key)
+ {
+  int s=0;
+  int e=n-1;
+  int mid = (s+e)/2;
+  
+  int ans=-1;
+  
+  while(s<=e)
+  {
+    if (arr[mid]==key)
+    {
+      ans=mid;
+      e=mid-1;
+
+    }
+
+    else if(key>arr[mid])
+    {
+
+      s=mid+1;
+    }
+    else if(key<arr[mid])
+    {
+
+      e=mid-1;
+    }
+
+    mid =s+(e-s)/2;
+
+  } 
+  return ans;
+  }
